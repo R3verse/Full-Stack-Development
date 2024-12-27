6434066162d35a68f503374922189fa4b50e8c72
R3verse <max@maxjensen.dk>
2024-12-27 16:08:29 +0100

    Array samples

--- NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/S1/hello-node.js
+++ NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)/S1/hello-node.js
@@ -1,1 +1,28 @@
-console.log("NodeJS server");
+const person = {
+  name: "Max",
+  age: 31,
+  greet() {
+    console.log(
+      "Hello, I'm " + this.name + " and I'm " + this.age + " years old."
+    );
+  },
+};
+
+const hobbies = ["Sports", "Cooking"];
+
+console.log(hobbies.map((hobby) => "Hobby: " + hobby));
+const copiedArray = [...hobbies];
+console.log(copiedArray);
+console.log(hobbies);
+console.log(person.name);
+
+const toArray = (...args) => {
+  return args;
+};
+
+console.log(toArray("example", "max", "1", "2"));
+const printName = ({ name, age }) => {
+  console.log(name, age);
+};
+
+printName(person);
